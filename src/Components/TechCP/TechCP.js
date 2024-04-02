11a2286bdbc6e47171be98783258e3f40b68314b
santicl <caballerosuarezsantiago@gmail.com>
2024-04-02 13:46:45 -0500

    Netlify toml

--- src/Components/TechCP/TechCP.js
+++ src/Components/TechCP/TechCP.js
@@ -1,24 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { addDays, format, endOfYear, startOfWeek } from 'date-fns';
 import assignTasks from './assignTasks';
 import '../../Components/TechCP/techcp.style.css';
 
 function TechCP() {
-    const groups = ['Salon Del Reino 1', 'Salon Del Reino 2', 'Torices', 'Papayal', 'Paseo Bolivar', 'Lo Amador', 'Nariño', 'G Posada 1', 'G Posada 2'];
+    // Definir los grupos
+    const groups = useMemo(() => ['Salon Del Reino 1', 'Salon Del Reino 2', 'Torices', 'Papayal', 'Paseo Bolivar', 'Lo Amador', 'Nariño', 'G Posada 1', 'G Posada 2'], []);
+
     const [assignments, setAssignments] = useState([]);
     const [showTable, setShowTable] = useState(false);
 
     useEffect(() => {
-        // Definir la fecha de inicio (primer día de la semana actual)
         let startDate = startOfWeek(new Date());
-        // Calcular la fecha final del año
         let endDate = endOfYear(new Date());
-        // Calcular la cantidad de semanas restantes en el año
         let weeks = Math.ceil((endDate - startDate) / (7 * 24 * 60 * 60 * 1000));
-        // Ejecutar la función para obtener las asignaciones
         const assignmentsData = assignTasks(addDays, startDate, weeks, groups);
         setAssignments(assignmentsData);
-    }, [groups]);
+    }, [groups]); // Agrega groups como dependencia
 
     function getDay(date) {
         const weekdays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
@@ -36,12 +34,12 @@ function TechCP() {
     };
 
     return (
-        <div style={{ maxWidth: '100%', overflowX: 'auto', margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ maxWidth: '100%', overflowX: 'auto', margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
             <h1 style={{ textAlign: 'center', fontSize: '24px', marginBottom: '20px', marginTop: '50px' }}>Tech Cleaner Program</h1>
-            <p style={{ textAlign: 'center', fontSize: '15px', marginBottom: '20px' }}>Este programa está desarrollado para asignar grupos de aseo durante el año disponible para la Congregación De Torices</p>
+            <p style={{ textAlign: 'center', fontSize: '15px', marginBottom: '20px' }}>Este programa está desarrollado para asignar grupos de aseo durante el año disponible</p>
             {showTable ? (
                 <>
-                    <button onClick={handleReloadClick} className="reload-button">Obtener de nuevo</button>
+                    <button onClick={handleReloadClick} style={ { width: '250px' } } className="reload-button">Obtener de nuevo</button>
                     <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid #ddd', fontFamily: 'Arial, sans-serif', marginBottom: '20px' }}>
                         <thead>
                             <tr>
